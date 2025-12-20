a63036e930f2bf338ab27602c7cca18589e6cb03
dominik.tch <dominik.tasch04@gmail.com>
2025-12-20 20:17:04 +0100

    Initial commit of blank JavaFX project

---
+++ product/Texas_Holdem_Poker/README.md
@@ -0,0 +1,14 @@
+[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/T_PJwcxm)
+# Exercise 7
+
+> Please add your solutions to all the Java Files provided. 
+
+> Graphical User Interface, JavaFX:
+> 
+> Model (Cadastre) 
+> 
+> View (cadastre-view.fxml)
+> 
+> Controller (CadastreController)
+
+> Add some notes here if you want.
---
+++ product/Texas_Holdem_Poker/src/main/java/at/ac/hcw/App.java
@@ -0,0 +1,23 @@
+package at.ac.hcw;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("cadastre-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1600, 900);
+        stage.setTitle("Texas Holdem Poker");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        launch();
+    }
+}
