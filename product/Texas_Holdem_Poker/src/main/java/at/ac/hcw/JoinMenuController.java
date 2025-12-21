7805a5a179c4e4e2d0056f27c64ddf77501821fe
dominik.tch <dominik.tasch04@gmail.com>
2025-12-21 15:04:25 +0100

    added onJoinButtonClicked function that tries to connect to the server via a provided JoinCode, finshed basic joinMenu UI with TextFiel an joinbutton

--- product/Texas_Holdem_Poker/src/main/java/at/ac/hcw/JoinMenuController.java
+++ product/Texas_Holdem_Poker/src/main/java/at/ac/hcw/JoinMenuController.java
@@ -1,10 +1,44 @@
 package at.ac.hcw;
-
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
 public class JoinMenuController {
+    @FXML
+    private TextField joinCodeField;
+    private PokerClient client;
+
+    @FXML
+    private void onJoinButtonClicked() {
+        String joinCode = joinCodeField.getText().trim();
+        if (joinCode.length() != 8) {
+            System.out.println("JoinCode muss 8 Zeichen haben");
+            //am besten ein Error ausgabe Feld in der UI implementieren und hier die Errorausgabe in das Feld geben
+            return;
+        }
+        String serverIP = JoinCodeHandler.joinCodeToIPv4(joinCode);
+        int serverPort = 5000; // fester Port
+
+        client = new PokerClient(serverIP, serverPort);
+
+        // Verbindung im Hintergrund-Thread, damit UI nicht einfriert
+        Task<Void> task = new Task<>() {
+            @Override
+            protected Void call() {
+                boolean connected = client.connect();
+                if (connected) {
+                    System.out.println("Erfolgreich verbunden!");
+                    // switch to LobbyMenu in the future
+                } else {
+                    System.out.println("Verbindung fehlgeschlagen!");
+                }
+                return null;
+            }
+        };
+        new Thread(task).start();
+    }
     @FXML
     private void onBackClicked() throws IOException {
         App.getSceneController().switchToMainMenu();
